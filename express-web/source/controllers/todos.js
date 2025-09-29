5c35223b973c4d64790e6c624663219fe5b39c7a
Denis Malorodov <denis.malod@mail.ru>
2025-09-29 16:46:02 +1000

    Добавить поиск

--- express-web/source/controllers/todos.js
+++ express-web/source/controllers/todos.js
@@ -1,8 +1,24 @@
 import { getList, getItem } from "../models/todos.js";
 
 export function mainPage(req, res) {
+  let list = getList();
+  if (req.query.search) {
+    const q = req.query.search.toLowerCase(); // ............... 3 
+        list = list.filter((el) => { 
+            if (el.title.toLowerCase().includes(q)) // ............. 4 
+                return true; 
+            else 
+                if (el.desc) 
+                    return el.desc.toLowerCase().includes(q); // ... 5 
+                else 
+                    return false; 
+        }); 
+
+  }
+
   res.render("main", {
-    todos: getList(),
+    req: req,
+    todos: list,
     title: "Главная",
   });
 }
