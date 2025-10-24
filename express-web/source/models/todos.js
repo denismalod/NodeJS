2edf9a9ef3a273cdc3d8c42c032b3c76b52c3afe
Denis Malorodov <denis.malod@mail.ru>
2025-10-24 14:12:44 +1000

    feat: методы модели

--- express-web/source/models/todos.js
+++ express-web/source/models/todos.js
@@ -1,21 +1,15 @@
 import { Todo } from "./__loaddatabase.js";
 
 export async function getList(user, doneAtLast, search) {
-    const qTodos = Todo.find({ user: user }); 
-    if (doneAtLast === '1') 
-        qTodos.sort('done createdAt'); 
-    else 
-        qTodos.sort('createdAt'); 
-    if (search) 
-        qTodos.or([ 
-            { title: new RegExp(search, 'i') }, 
-            { desc: new RegExp(search, 'i') } 
-        ]); 
-    return await qTodos; 
+  const qTodos = Todo.find({ user: user });
+  if (doneAtLast === "1") qTodos.sort("done createdAt");
+  else qTodos.sort("createdAt");
+  if (search) qTodos.contains(search);
+  return await qTodos;
 }
 
 export async function getItem(id, user) {
-  return await Todo.findOne({ _id: id, user: user }); 
+  return await Todo.findOne({ _id: id, user: user });
 }
 
 export async function addItem(todo) {
@@ -24,52 +18,46 @@ export async function addItem(todo) {
 }
 
 export async function setDoneItem(id, user) {
-  const todo = await getItem(id, user);
-  if (todo) {
-    todo.done = true;
-    await todo.save();
-    return true;
-  } else return false;
+  return await Todo.findOneAndSetDone(id, user);
 }
 
 export async function deleteItem(id, user) {
-  return await Todo.findOneAndDelete({ _id: id, user: user }); 
+  return await Todo.findOneAndDelete({ _id: id, user: user });
 }
 
+export async function getMostActiveUsers() {
+  const result = [];
 
-export async function getMostActiveUsers() { 
-    const result = []; 
- 
-    result[0] = await Todo.aggregate([ 
-        { 
-            $lookup: { 
-                from: 'users', 
-                localField: 'user', 
-                foreignField: '_id', 
-                as: 'userObj' 
-            } 
-        }, 
-        { $unwind: '$userObj' }, 
-        { $group: { _id: '$userObj.username', cnt: { $count: {} } } }, 
-        { $sort: { cnt: -1 } }, 
-        { $limit: 3 } 
-    ]); 
- 
-    result[1] = await Todo.aggregate([ 
-        { $match: { done: true } }, 
-        { 
-            $lookup: { 
-                from: 'users', 
-                localField: 'user', 
-                foreignField: '_id', 
-                as: 'userObj' 
-            } 
-        }, 
-        { $unwind: '$userObj' }, 
-        { $group: { _id: '$userObj.username', cnt: { $count: {} } } }, 
-        { $sort: { cnt: -1 } }, 
-        { $limit: 3 } 
-    ]); 
- 
-    return result; 
-} 
+  result[0] = await Todo.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "userObj",
+      },
+    },
+    { $unwind: "$userObj" },
+    { $group: { _id: "$userObj.username", cnt: { $count: {} } } },
+    { $sort: { cnt: -1 } },
+    { $limit: 3 },
+  ]);
+
+  result[1] = await Todo.aggregate([
+    { $match: { done: true } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "userObj",
+      },
+    },
+    { $unwind: "$userObj" },
+    { $group: { _id: "$userObj.username", cnt: { $count: {} } } },
+    { $sort: { cnt: -1 } },
+    { $limit: 3 },
+  ]);
+
+  return result;
+}
