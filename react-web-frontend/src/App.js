dce8f30851bf4afa76f562d6159704dc366fe24e
Denis Malorodov <denis.malod@mail.ru>
2025-11-05 14:24:28 +1000

    Компоненты регистрации и добавления дела

--- react-web-frontend/src/App.js
+++ react-web-frontend/src/App.js
@@ -1,10 +1,12 @@
 import TodoList from "./TodoList.js";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./Login.js";
 import Logout from "./Logout.js";
 import { TokenContext } from "./utility.js";
-import TodoDetail from './TodoDetail.js'; 
+import TodoDetail from "./TodoDetail.js";
+import TodoAdd from "./TodoAdd.js";
+import Register from "./Register.js";
 
 function App() {
   const [token, setToken] = useState(undefined);
@@ -14,12 +16,12 @@ function App() {
   }
 
   return (
-    <BrowserRouter> 
+    <BrowserRouter>
       <nav>
         {token && (
-           <Link to="/" className="brand"> 
+          <Link to="/" className="brand">
             <span>Список дел</span>
-          </Link> 
+          </Link>
         )}
 
         <input id="bmenub" type="checkbox" className="show" />
@@ -27,25 +29,35 @@ function App() {
           &#9776;
         </label>
         <div className="menu">
+          {token && (
+            <Link to="/add/" className="button">
+              Добавить дело
+            </Link>
+          )}
           {token && <Logout acceptToken={acceptToken} />}
+          {!token && (
+            <Link to="/register/" className="button">
+              Зарегистрироваться
+            </Link>
+          )}
+          {!token && (
+            <Link to="/login/" className="button success">
+              Войти
+            </Link>
+          )}
         </div>
       </nav>
       <TokenContext.Provider value={token}>
-              <Routes> 
-                  <Route path="/" element={ 
-                      <TodoList /> 
-                  } /> 
-                  <Route path="/:id" element={ 
-                      <TodoDetail /> 
-                  } /> 
-                  <Route path="/login" element={ 
-                      <Login acceptToken={acceptToken} /> 
-                  } /> 
-              </Routes> 
-
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/:id" element={<TodoDetail />} />
+          <Route path="/login" element={<Login acceptToken={acceptToken} />} />
+          <Route path="/add" element={<TodoAdd />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </TokenContext.Provider>
       <p className="copyright">Все права принадлежат читателю книги.</p>
-    </BrowserRouter> 
+    </BrowserRouter>
   );
 }
 
